63ea93fb996ed813b7bf6890d66790c09c99a17d
yajamon <yajamon.tatsuki@gmail.com>
2015-07-01 17:59:55 +0900

    Add base file

---
+++ YJMActionSheet.h
@@ -0,0 +1,13 @@
+//
+//  YJMActionSheet.h
+//  YJMActionSheetDemo
+//
+//  Created by yajamon on 2015/07/01.
+//  Copyright (c) 2015年 yajamon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YJMActionSheet : NSObject
+
+@end
---
+++ YJMActionSheet.m
@@ -0,0 +1,13 @@
+//
+//  YJMActionSheet.m
+//  YJMActionSheetDemo
+//
+//  Created by yajamon on 2015/07/01.
+//  Copyright (c) 2015年 yajamon. All rights reserved.
+//
+
+#import "YJMActionSheet.h"
+
+@implementation YJMActionSheet
+
+@end
